5db58ceaaa5f2bd68e439a758521ab6057f8ebcd
Tunaki <guillaume_boue@orange.fr>
2016-02-27 00:15:18 +0100

    The id is retrieved from the Share URL so that it lao works on site where user doesn't have an account

--- showtimeline.user.js
+++ showtimeline.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show Timeline
 // @namespace    https://github.com/Tunaki/stackoverflow-userscripts
-// @version      0.1
+// @version      0.2
 // @description  Adds an anchor below posts that links to their timeline
 // @author       Tunaki
 // @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/q(uestions)?\/\d+/
@@ -9,12 +9,11 @@
 // ==/UserScript==
 
 $('.post-menu').each(function() {
-  var $this = $(this),
-      answerId = $this.find('a.flag-post-link').attr('data-postid');
+  var $this = $(this);
+  var answerId = $this.find('a.short-link').attr('id').split('-')[2];
   $this.append($('<span>').attr('class', 'lsep').html('|'));
   $this.append($('<a>').attr({
-    'class': 'short-link', 
-    'href': '/posts/' + answerId + '/timeline'}
-  ).html('timeline'));
+    'class': 'timeline-link', 
+    'href': '/posts/' + answerId + '/timeline'
+  }).html('timeline'));
 });
-
