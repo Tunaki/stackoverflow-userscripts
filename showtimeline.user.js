a1f74bbe0a664d64e6841376188d9e2d954cd90a
Tunaki <guillaume_boue@orange.fr>
2016-03-05 15:45:39 +0100

    Add callback to load event so that the timeline shows on dynamically loaded posts

--- showtimeline.user.js
+++ showtimeline.user.js
@@ -1,19 +1,39 @@
 // ==UserScript==
 // @name         Show Timeline
 // @namespace    https://github.com/Tunaki/stackoverflow-userscripts
-// @version      0.2
+// @version      0.3
 // @description  Adds an anchor below posts that links to their timeline
 // @author       Tunaki
-// @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/q(uestions)?\/\d+/
+// @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/.*/
 // @grant        none
 // ==/UserScript==
 
-$('.post-menu').each(function() {
-  var $this = $(this);
-  var postId = $this.find('a.short-link').attr('id').split('-')[2];
-  $this.append($('<span>').attr('class', 'lsep').html('|'));
-  $this.append($('<a>').attr({
-    'class': 'timeline-link', 
-    'href': '/posts/' + postId + '/timeline'
-  }).html('timeline'));
+let funcs = {};
+
+funcs.addXHRListener = callback => {
+    let open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        this.addEventListener('load', callback.bind(null, this), false);
+        open.apply(this, arguments);
+    };
+};
+
+funcs.addXHRListener(xhr => {
+    if (/ajax-load-realtime/.test(xhr.responseURL)) {
+        addTimelineLink();
+    }
 });
+
+function addTimelineLink() {  
+  $('.post-menu:not(:has(.timeline-link))').each(function() {
+    var $this = $(this);
+    var postId = $this.find('a.short-link').attr('id').split('-')[2];
+    $this.append($('<span>').attr('class', 'lsep').html('|'));
+    $this.append($('<a>').attr({
+      'class': 'timeline-link', 
+      'href': '/posts/' + postId + '/timeline'
+    }).html('timeline'));
+  });
+}
+
+addTimelineLink();
