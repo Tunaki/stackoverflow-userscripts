8c5e2d3b1691187954bec48f71c872a7085100a2
Tunaki <guillaume_boue@orange.fr>
2016-08-10 00:31:29 +0200

    Fixing comment flagging bug when loading answers

--- reflagreview.user.js
+++ reflagreview.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Reflag Review
 // @namespace    https://github.com/Tunaki/stackoverflow-userscripts
-// @version      0.2
-// @description  In the flag history, keeps only NAA/VLQ helpful-flagged answers and loads them to review and potentially ask for reflags
+// @version      0.2.1
+// @description  In the flag history, keeps only NAA/VLQ helpful-flagged on non-deleted answers and loads them to review and potentially ask for reflags
 // @author       Tunaki
 // @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/users\/flag-summary\/\d+/
 // @grant        none
@@ -12,35 +12,39 @@ StackExchange.using("inlineEditing", function () {
   StackExchange.inlineEditing.init();
 });
 
+var time = 0;
+
 $('#mainbar.user-flag-history').prepend(
   $('<div>').append(
     $('<a>').attr('href', '#').click(function() {
       $('.flagged-post').hide();
-      $('.flagged-post').filter(':has(.Helpful):not(:has(.deleted-answer))').filter(function() {
+      var $posts = $('.flagged-post').filter(':has(.Helpful):not(:has(.deleted-answer))').filter(function() {
         return $.inArray($('.revision-comment', this).html(), ['not an answer', 'very low quality']) != -1 && $('.mod-flag', this).length == 1;
-      }).each(function(index) {
+      });
+      var length = $posts.length;
+      $posts.each(function(index) {
         var href = $('.answer-hyperlink', this).attr('href');
-        loadAnswerInto(index, href.split('#')[1], href.split('\/')[2], $(this));
+        setTimeout(loadAnswerInto, time, index === length - 1, href.split('#')[1], href.split('\/')[2], $(this));
+        time += 500;
       }).show();
       $(this).remove();
     }).html('reflags')
   ).css('padding-bottom', '10px')
 );
 
-function loadAnswerInto(index, answerId, questionId, $element) {
+function loadAnswerInto(last, answerId, questionId, $element) {
   $.get('/posts/' + questionId + '/votes', function(votes) {
     $.get('/posts/ajax-load-realtime/' + answerId, function(data) {
       $element.html(data);
-      if (index == 0) {        
+      if (last) {        
         StackExchange.question.init({
           votesCast: votes,
           canViewVoteCounts: true,
-          questionId: questionId,
-          canOpenBounty: true
+          questionId: questionId
         });
+        $('.js-show-link.comments-link').click();
       }
       StackExchange.realtime.subscribeToQuestion(StackExchange.options.site.id, questionId);
-      $element.find('.js-show-link.comments-link').click();
     });
   });
 }
